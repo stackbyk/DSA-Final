f1c2bd5b96df8adacc51b8c56ed8d1caba76b7f8
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2026-08-25 21:16:40 +0530

    Move Zeroes

---
+++ Two Pointers Arr/Move Zeroes Brute.java
@@ -0,0 +1,26 @@
+package Two Pointers;
+import java.uitl.*;
+public class Move Zeroes Brute {
+    public static void MoveZeroes(int[]arr){
+        int n=arr.length;
+        int[]temp=new int[n];
+        int j=0;
+        
+        // 1.Create a temp arr-> store non zero elems
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                temp[j]=arr[i];
+                j++; //increment to move on
+            }
+        }
+        // 2.copy temp arr to original arr
+        for(int i=0;i<n;i++){
+            arr[i]=temp[i];
+        }
+    }
+    public static void main(String[]args){
+        int[]arr={0, 1, 0, 3, 12};
+        MoveZeroes(arr);
+        System.out.println("The new array is:"+Arrays.toString(arr));
+    }
+}
